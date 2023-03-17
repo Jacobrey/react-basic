adef2ba0d55d4c3ae83f5f54efd1e10b815a3595
Jacobrey <1162161624@qq.com>
2023-03-17 19:47:52 +0800

    初始化项目

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
 function App() {
   return (
-    <div className="App">
-      <h1>根组件</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link>
+        {/* 配置路由规则 */}
+        <Routes>
+          <Route path="/home" element={<Layout></Layout>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
